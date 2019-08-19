5561101016ef33ed3af2a9c5b901d609d5fc82b2
pangshuhai <pangshuhai@126.com>
2019-08-19 10:20:02 +0800

    no message

---
+++ info4u/pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <nav class="nav">
+      <header class="Header_container">
+        <div class="Header_nav_lf">
+          <a href="#">Info4u</a>
+          <nuxt-link :to="{name: 'MobilePage'}">about</nuxt-link>
+        </div>
+        <div class="Header_nav_rt"></div>
+      </header>
+    </nav>
+    <div class="nav__placeholder"></div>
+    <div class="home_ad"></div>
+    <div class="home_timeline">
+      <el-calendar v-model="value"></el-calendar>
+      <!-- <no-ssr><el-calendar v-model="value"></el-calendar></no-ssr> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: new Date()
+    }
+  },
+  beforeMount () {
+    // this.$router.redirect({path: 'mobile'})
+  },
+  methods: {
+    load () {
+      // this.count += 2
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+}
+.nav__placeholder {
+  height: 58px;
+}
+.nav {
+  position: fixed;
+  width: 100%;
+  left: 0;
+  top: 0;
+  background-color: #1369bf;
+  color: #fff;
+  z-index: 999;
+}
+</style>
---
+++ info4u/pages/mobile/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <nav class="nav">2</nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MobilePage',
+  data() {
+    return {
+      app: 'b'
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+}
+</style>
